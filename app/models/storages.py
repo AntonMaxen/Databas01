b489b8b71d7acbdfe12cef795dc2a541b8e10034
Måns Ewards Öberg <ewardsoberg@gmail.com>
2020-11-13 11:47:10 +0100

    Storages Module

--- app/models/storages.py
+++ app/models/storages.py
@@ -6,13 +6,14 @@ from sqlalchemy.orm import relationship
 class Storage(Base):
     __tablename__ = 'storages'
 
-    id = sa.Column(sa.Integer, primary_key=True)
+    id = sa.Column(sa.Integer, foreign_key=True)
     product_amount = sa.Column(sa.String(45), nullable=False)
     min_amount = sa.Column(sa.String(100), nullable=False)
     reorder_amount = sa.Column(sa.Integer, nullable=False)
     shop_id = sa.Column(sa.Integer, sa.ForeignKey('shops.id'))
     shop = relationship('Shop', back_populates='storage')
-    # TODO: Make one->one relations work with shop
+
 
     def __repr__(self):
-        return f'Car(=license_number{self.license_number} model_name={self.model_name}, prod_year={self.prod_year})'
+        return f'Storage(id={self.id}, product_amount={self.product_amount}, min_amount={self.min_amount},' \
+               f'reorder_amount={self.reorder_amount}, shop_id={self.shop_id}, shop={self.shop})'
