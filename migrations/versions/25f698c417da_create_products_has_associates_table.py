d94ec5e4a2bd12f1540f6ad4e009f1f0766a7f80
Anton Maxén <Anton.maxen@gmail.com>
2020-11-16 18:02:12 +0100

    updated products has associates migration file

--- migrations/versions/25f698c417da_create_products_has_associates_table.py
+++ migrations/versions/25f698c417da_create_products_has_associates_table.py
@@ -7,6 +7,7 @@ Create Date: 2020-11-16 14:28:22.565946
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.engine.reflection import Inspector
 
 
 # revision identifiers, used by Alembic.
@@ -17,8 +18,16 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        'products_has_associates',
+        sa.Column("ProductId", sa.Integer, sa.ForeignKey('products.id'), primary_key=True),
+        sa.Column("AssociateId", sa.Integer, sa.ForeignKey('associates.id'), primary_key=True)
+    )
 
 
 def downgrade():
-    pass
+    conn = op.get_bind()
+    inspector = Inspector.from_engine(conn)
+    tables = inspector.get_table_names()
+    if 'products_has_associates' in tables:
+        op.drop_table('products_has_associates')
