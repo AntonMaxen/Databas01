08ca9efab329053d5a4aacfb6c1a462dc97f2cc6
Måns Ewards Öberg <ewardsoberg@gmail.com>
2020-11-16 16:03:22 +0100

    Contact_persons migration

--- migrations/versions/d021d31441aa_create_contact_persons_table.py
+++ migrations/versions/d021d31441aa_create_contact_persons_table.py
@@ -10,6 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
+from sqlalchemy.engine.reflection import Inspector
+
 revision = 'd021d31441aa'
 down_revision = 'efdf44b6f92b'
 branch_labels = None
@@ -17,8 +19,19 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        'contact_persons',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(45), nullable=False),
+        sa.Column('phone', sa.String(45), nullable=False),
+        sa.Column('email', sa.String(45), nullable=False),
+        sa.Column('category', sa.String(45), nullable=False)
+    )
 
 
 def downgrade():
-    pass
+    conn = op.get_bind()
+    inspector = Inspector.from_engine(conn)
+    tables = inspector.get_table_names()
+    if 'contact_persons' in tables:
+        op.drop_table('contact_persons')
