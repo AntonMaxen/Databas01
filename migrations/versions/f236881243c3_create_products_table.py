bf5c443a7ca0addddb9532cc8099f437dae1b9f4
Anton Maxén <Anton.maxen@gmail.com>
2020-11-16 15:57:15 +0100

    added upgrade and downgrade to create_products table migration

--- migrations/versions/f236881243c3_create_products_table.py
+++ migrations/versions/f236881243c3_create_products_table.py
@@ -7,6 +7,7 @@ Create Date: 2020-11-16 14:26:03.414234
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.engine.reflection import Inspector
 
 
 # revision identifiers, used by Alembic.
@@ -17,8 +18,19 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        "products",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("product_name", sa.String(100), nullable=False),
+        sa.Column("description", sa.String(100), nullable=False),
+        sa.Column("purchase_price", sa.String(45), nullable=False),
+        sa.Column("retail_price", sa.String(100), nullable=False)
+    )
 
 
 def downgrade():
-    pass
+    conn = op.get_bind()
+    inspector = Inspector.from_engine(conn)
+    tables = inspector.get_table_names()
+    if 'products' in tables:
+        op.drop_table('products')
