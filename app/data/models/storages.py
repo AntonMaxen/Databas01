889a03eace20af5e10bfe51e536591ce55474d79
Måns Ewards Öberg <ewardsoberg@gmail.com>
2020-11-17 13:37:09 +0100

    storages fixes

--- app/data/models/storages.py
+++ app/data/models/storages.py
@@ -10,10 +10,9 @@ class Storage(Base):
     product_amount = sa.Column(sa.String(45), nullable=False)
     min_amount = sa.Column(sa.String(100), nullable=False)
     reorder_amount = sa.Column(sa.Integer, nullable=False)
-    shop_id = sa.Column(sa.Integer, sa.ForeignKey('shops.id'))
-    shops = relationship('Shop', back_populates='storages')
-    products = relationship('Product', back_populates='storages')
+    shops_has_storages = relationship('ShopStorage', back_populates='storages')
 
     def __repr__(self):
         return f'Storage(id={self.id}, product_amount={self.product_amount}, min_amount={self.min_amount},' \
-               f'reorder_amount={self.reorder_amount}, shop_id={self.shop_id}, shop={self.shop})'
+               f'reorder_amount={self.reorder_amount}, shop_id={self.shop_id}, shop={self.shop},' \
+               f'shop_has_storages={self.shops_has_storages})'
