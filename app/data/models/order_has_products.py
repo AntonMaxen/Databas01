89be1bdf72a5f3c17dbff38d1e9e92b18f8cdb97
Anton Maxén <Anton.maxen@gmail.com>
2020-11-17 11:21:20 +0100

    added order has products model

---
+++ app/data/models/order_has_products.py
@@ -0,0 +1,16 @@
+from app.data.db import Base
+import sqlalchemy as sa
+from sqlalchemy.orm import relationship
+
+
+class OrderProduct(Base):
+    __tablename__ = 'order_has_products'
+    OrderId = sa.Column(sa.Integer, sa.ForeignKey('orders.id'), primary_key=True),
+    ProductId = sa.Column(sa.Integer, sa.ForeignKey('products.id'), primary_key=True),
+    Amount = sa.Column(sa.Integer, nullable=False)
+    products = relationship('Product', back_populates='order_has_products')
+    orders = relationship('Order', back_populates='order_has_products')
+
+    def __repr__(self):
+        return f'OrderProduct(OrderId={self.OrderIdId} ProductId={self.ProductId}' \
+               f'Amount={self.Amount})'
