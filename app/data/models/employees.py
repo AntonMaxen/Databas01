884f3ab6dfac919e55dcdec1f4555a0b23b75c7f
Måns Ewards Öberg <ewardsoberg@gmail.com>
2020-11-17 13:05:12 +0100

    employees fixes

--- app/data/models/employees.py
+++ app/data/models/employees.py
@@ -14,10 +14,10 @@ class Employee(Base):
     email = sa.Column(sa.String(45), nullable=False)
     reports_to = sa.Column(sa.Integer, nullable=False)
     shop_id = sa.Column(sa.Integer, sa.ForeignKey('shops.id'))
-    shop = relationship('Shop', back_populates='employee')
-    orders = relationship("Order", back_populates="employee")
+    shops = relationship('Shop', back_populates='employees')
+    orders = relationship("Order", back_populates="employees")
 
     def __repr__(self):
         return f'Employee(id={self.id}, first_name={self.first_name}, last_name={self.last_name},' \
                f' job_title={self.job_title}, email={self.email}, reports_to={self.reports_to},' \
-               f' shop_id={self.shop_id}, shop={self.shop}, orders={self.orders})'
+               f' shop_id={self.shop_id}, shops={self.shops}, orders={self.orders})'
