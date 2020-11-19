cec5d6500c8afdb3dc18086b96bd8171d94e1b47
Måns Ewards Öberg <ewardsoberg@gmail.com>
2020-11-19 16:22:59 +0100

    Shop generator + menu

--- app/data/repository/shops_repo.py
+++ app/data/repository/shops_repo.py
@@ -1,6 +1,14 @@
-def main():
-    pass
+from data.models.shops import Shop
+from data.db import session
 
 
-if __name__ == "__main__":
-    main()
+def show_all_shops():
+    return session.query(Shop).all()
+
+
+def show_shop_address(shopid):
+    return session.query(Shop).filter(Shop.id == shopid, Shop.address_line_one, Shop.city, Shop.country)
+
+
+def show_shop_phone(shopid):
+    return session.query(Shop).filer(Shop.id == shopid, Shop.phone)
