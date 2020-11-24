ead7867f2a206b4ddc09a9ecf77f2d7b88d8e73c
Marcus Johnsson <mr.marcus.marega@gmail.com>
2020-11-24 16:08:42 +0100

    built the UI --> DB for products. Broke out UI funcs into ui_utils.py to DRY it up a bit. Since some agreed upon conventions arent implemented I expect some comments =D

---
+++ app/BL/product_controller.py
@@ -0,0 +1,42 @@
+import app.data.repository.products_repo as pr
+from app.BL.utils import modelobj_to_dict
+
+
+def get_all_products():
+    products = pr.get_all_products()
+    return [modelobj_to_dict(product) for product in products]
+
+
+def get_product_by_id(p_id):
+    product = pr.get_product_by_id(p_id)
+    return product, modelobj_to_dict(product)
+
+
+def get_products_by_name(p_name):
+    products = pr.get_products_by_columnvalue("product_name", p_name)
+    return [modelobj_to_dict(product) for product in products]
+
+
+def get_products_by_columnvalue(column_name, value):
+    products = pr.get_products_by_columnvalue(column_name, value)
+    return [modelobj_to_dict(product) for product in products]
+
+
+def get_columns():
+    return pr.get_columns()
+
+
+def update_product_column(product, column, value):
+    pr.update_product_column(product, column, value)
+
+
+def add_product(insert_dict):
+    pr.add_product(insert_dict)
+
+
+def delete_product(t_id):
+    pr.delete_product(t_id)
+
+
+if __name__ == '__main__':
+    pass
--- app/data/repository/products_repo.py
+++ app/data/repository/products_repo.py
@@ -1,3 +1,37 @@
+from app.data.db import session
+from app.data.model_imports import *
+import app.data.repository.table_functions as tf
+from sqlalchemy.exc import DatabaseError, CircularDependencyError, TimeoutError
+
+
+def get_all_products():
+    return tf.get_all_assets(Product)
+
+
+def get_product_by_id(p_id, column_name='id'):
+    return tf.get_asset_by_id(Product, p_id, column_name=column_name)
+
+
+def get_products_by_columnvalue(column_name, value):
+    return tf.get_assets_by_columnvalue(Product, column_name, value)
+
+
+def get_columns():
+    return tf.get_columns(Product)
+
+
+def update_product_column(product_obj, column_name, value):
+    tf.update_asset_by_column(product_obj, column_name, value)
+
+
+def add_product(insert_dict):
+    tf.add_row(Product, insert_dict)
+
+
+def delete_product(t_id, column_name="id"):
+    tf.drop_row_by_id(Product, t_id, column_name=column_name)
+
+
 def main():
     pass
 
