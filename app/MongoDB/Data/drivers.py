64133d75a6f293e6a84b1f0a07808123b573e863
Måns Ewards Öberg <ewardsoberg@gmail.com>
2020-12-08 14:56:00 +0100

    order mongo

--- app/MongoDB/Data/drivers.py
+++ app/MongoDB/Data/drivers.py
@@ -144,7 +144,6 @@ def fix_customers():
         mm.Customer(mongo_customer).save()
 
 
-
 def fix_orders():
     orders = session.query(Order).all()
     customers = session.query(Customer).all()
@@ -153,19 +152,19 @@ def fix_orders():
         order_dict = order.__dict__
         order_dict['employee_id'] = mm.Employee.find(id=order.employee_id).first_or_none()._id
         del order_dict['_sa_instance_state']
-        order_dict['customer_info'] = []
+        customer_id = mm.Customer.find(id=order.customer_id).first_or_none()._id
         for customer in customers:
             if order_dict['customer_id'] == customer.id:
-                order_dict['customer_info'].append({
-                    'customer_id': customer.id,
+                order_dict['customer_info'] = ({
+                    'customer_id': customer_id,
                     'first_name': customer.first_name,
                     'last_name': customer.last_name,
                     'address': customer.address_line_one,
                     'phone': customer.phone
                 })
 
-        del order_dict['customer_id']
         del order_dict['id']
+        del order_dict['customer_id']
 
         mongo_orders = mm.Order(order_dict)
         mongo_orders.save()
